24989a8b7991c4b2cee6e755f9498fe549a9fb3e
Rvendell <2312237896@qq.com>
2020-04-17 16:43:01 +0800

    Create 074search-a-2d-matrix.cpp

---
+++ code/074search-a-2d-matrix.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		//基本思想：二分查找，将二维数组matrix看成一维数组，下标为mid的值在二维数组对应值为matrix[mid/m][mid%m]
+		if (matrix.size() == 0)
+			return false;
+		int low, mid, high;
+		int n = matrix.size(), m = matrix[0].size();
+		low = 0;
+		high = n * m - 1;
+		while (low <= high)
+		{
+			mid = (low + high) / 2;
+			if (target == matrix[mid / m][mid % m])
+				return true;
+			else if (target > matrix[mid / m][mid % m])
+			{
+				low = mid + 1;
+			}
+			else
+				high = mid - 1;
+		}
+		return false;
+	}
+};
+int main()
+{
+	Solution solute;
+	vector<vector<int>> matrix = {
+		{1,   3,  5,  7} ,
+		{10, 11, 16, 20} ,
+		{23, 30, 34, 50}
+	};
+	int target = 3;
+	cout << solute.searchMatrix(matrix, target) << endl;
+	return 0;
+}
