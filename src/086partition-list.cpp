2b38846fb2cb5a041f2a255609df9d4e0728c501
Hutao <2312237896@qq.com>
2021-03-12 19:43:32 +0800

    Update 086partition-list.cpp

--- src/086partition-list.cpp
+++ src/086partition-list.cpp
@@ -26,6 +26,31 @@ void Init_ListNode(ListNode* head, vector<int> vec)
 }
 class Solution {
 public:
+    ListNode* partition(ListNode* head, int x) {
+		//简化版：单链表操作，链表start1中元素都小于x，链表start2中元素都大于等于x，最后合并两个链表
+		ListNode* start1=new ListNode(0),*start2=new ListNode(0);
+		ListNode* p=start1,*q=start2;
+		while(head)
+		{
+			if(head->val<x)
+			{
+				p->next=head;
+				p=p->next;
+			}
+			else
+			{
+				q->next=head;
+				q=q->next;
+			}
+			head=head->next;
+		}
+		p->next=start2->next;
+		q->next=nullptr;
+		return start1->next;
+    }
+};
+class Solution1 {
+public:
 	ListNode* partition(ListNode* head, int x) {
 		//基本思想：单链表操作，p指向小于x的右边界节点，q指向大于等于x的右边界节点，cur指向当前节点
 		//如果当前节点cur小于x，将cur节点插到p的位置
