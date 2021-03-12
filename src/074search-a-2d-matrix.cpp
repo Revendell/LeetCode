2b058982124483318dfe23a0fa61b849b4741699
Hutao <2312237896@qq.com>
2021-03-12 11:57:40 +0800

    Update 074search-a-2d-matrix.cpp

--- src/074search-a-2d-matrix.cpp
+++ src/074search-a-2d-matrix.cpp
@@ -5,6 +5,24 @@ using namespace std;
 class Solution {
 public:
 	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		//简化版：从左下角出发，比target大就上移，比target小就下移
+		int n = matrix.size(), m = matrix[0].size();
+		int i=n-1,j=0;
+		while(i<0||j>=m)
+		{
+			if(matrix[i][j]==target)
+				return true;
+			else if(matrix[i][j]>target)
+				i--;
+			else
+				j++;
+		}
+		return false;
+	}
+};
+class Solution1 {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
 		//基本思想：二分查找，将二维数组matrix看成一维数组，下标为mid的值在二维数组对应值为matrix[mid/m][mid%m]
 		if (matrix.size() == 0)
 			return false;
