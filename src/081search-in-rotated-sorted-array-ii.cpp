7e4f27191a1403e2051ada34983904368ecd3b82
Hutao <2312237896@qq.com>
2021-03-12 16:20:51 +0800

    Update 081search-in-rotated-sorted-array-ii.cpp

--- src/081search-in-rotated-sorted-array-ii.cpp
+++ src/081search-in-rotated-sorted-array-ii.cpp
@@ -5,6 +5,41 @@ using namespace std;
 class Solution {
 public:
 	bool search(vector<int>& nums, int target) {
+		//简化版：二分查找，旋转点将nums分成两部分，先判断mid在左半部分还是右半部分，之后mid又将剩下部分分成两部分
+		//如果10111和11101这种情况下 nums[low] == nums[mid]：low++
+		//如果在左半部分根据nums[low]<target&&target<nums[mid]判断位于mid的左边
+		//如果在右半部分根据nums[mid]<target&&target<=nums[high]判断位于mid的右边
+		int n = nums.size();
+		if(n==0)  return -1;
+		int low=0, mid, high=n-1;
+		while(low<=high)
+		{
+			mid=(low+high)/2;
+			if(nums[mid]==target)
+				return true;
+			else if(nums[low]==nums[mid])
+				low++;
+			else if(nums[low]<nums[mid])
+			{
+				if(nums[low]<=target&&target<nums[mid])
+					high=mid-1;
+				else
+					low=mid+1;
+			}
+			else
+			{
+				if(nums[mid]<target&&target<=nums[high])
+					low=mid+1;
+				else
+					high=mid-1;
+			}
+		}
+		return false;
+	}
+};
+class Solution1 {
+public:
+	bool search(vector<int>& nums, int target) {
 		//基本思想：折半查找
 		int low, high, mid;
 		low = 0;
