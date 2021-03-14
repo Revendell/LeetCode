86f1908c3eda70ef9acbc3b89131f4bee1efaa55
Hutao <2312237896@qq.com>
2021-03-14 19:24:50 +0800

    Update 130surrounded-regions.cpp

--- src/130surrounded-regions.cpp
+++ src/130surrounded-regions.cpp
@@ -7,6 +7,47 @@ public:
 	int flag = 0;  //标记连通的O是否被X包围，0是包围，1是没有包围(当连通的O存在边界上时flag=1)
 	vector<vector<int>> pos;  //记录连通的O的位置，如果flag=0将这些位置的O置位X
 	void solve(vector<vector<char>>& board) {
+		//简化版：深度优先搜索
+		vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(), false));
+		for (int i = 0; i < board.size(); i++)
+		{
+			for (int j = 0; j < board[i].size(); j++)
+			{
+				if (board[i][j] == 'O' && !visited[i][j])
+				{
+					Recursion(board, i, j, visited);
+					if (flag == 0)
+					{
+						for(auto v:pos)
+							board[v[0]][v[1]] = 'X';
+					}
+					flag = 0;
+					pos.clear();
+				}
+			}
+		}
+	}
+	void Recursion(vector<vector<char>>& board, int i, int j, vector<vector<bool>>& visited)
+	{
+		if(i<0||i>=board.size()||j<0||j>=board[i].size())
+			return;
+		if(board[i][j] != 'O' || visited[i][j])
+			return;
+		pos.push_back({ i,j });
+		visited[i][j] = true;
+		if (i == 0 || i == board.size() - 1 || j == 0 || j == board[i].size() - 1)
+			flag = 1;
+		Recursion(board, i - 1, j, visited);
+		Recursion(board, i + 1, j, visited);
+		Recursion(board, i, j - 1, visited);
+		Recursion(board, i, j + 1, visited);
+	}
+};
+class Solution1 {
+public:
+	int flag = 0;  //标记连通的O是否被X包围，0是包围，1是没有包围(当连通的O存在边界上时flag=1)
+	vector<vector<int>> pos;  //记录连通的O的位置，如果flag=0将这些位置的O置位X
+	void solve(vector<vector<char>>& board) {
 		//基本思想：dfs，遍历非边界所有的O，然后dfs判断查找连通的O是否存在位于边界的O，存在的话flag=1
 		if (board.size() == 0)
 			return;
