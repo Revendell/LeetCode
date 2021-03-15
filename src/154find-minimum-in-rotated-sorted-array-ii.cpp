406916f5c555aca3f2e452002a3c9ac210144dfe
Hutao <2312237896@qq.com>
2021-03-15 20:00:45 +0800

    Update 154find-minimum-in-rotated-sorted-array-ii.cpp

--- src/154find-minimum-in-rotated-sorted-array-ii.cpp
+++ src/154find-minimum-in-rotated-sorted-array-ii.cpp
@@ -4,25 +4,21 @@
 using namespace std;
 class Solution {
 public:
-	int findMin(vector<int>& nums) {
-		//基本思想：折半查找
-		int low = 0, high = nums.size() - 1;
-		while (high >= 0 && nums[low] == nums[high])
-			high--;
-		while (low < high)
-		{
-			int mid = (low + high) / 2;
-			if (nums[mid] <= nums[high])
-			{
-				high = mid;
-			}
-			else
-			{
-				low = mid + 1;
-			}
-		}
-		return nums[low];
-	}
+    int findMin(vector<int>& nums) {
+		//基本思想：二分查找
+        int left=0,right=nums.size()-1;
+        while(left<right)
+        {
+            int mid=(left+right)/2;
+            if(nums[right]==nums[mid])
+                right--;
+            else if(nums[right]<nums[mid])
+                left=mid+1;
+            else
+                right=mid;
+        }
+        return nums[left];
+    }
 };
 int main()
 {
