03edd419c7ac580c9a6f1c5c84bc9d877f92cdc4
Hutao <2312237896@qq.com>
2021-03-16 10:20:55 +0800

    Update 200number-of-islands.cpp

--- src/200number-of-islands.cpp
+++ src/200number-of-islands.cpp
@@ -15,23 +15,23 @@ public:
 				if (grid[i][j] == '1')
 				{
 					res++;
-					Recursion(grid, i, j);
+					dfs(grid, i, j);
 				}
 			}
 		}
 		return res;
 	}
-	void Recursion(vector<vector<char>>& grid, int i, int j)
+	void dfs(vector<vector<char>>& grid, int i, int j)
 	{
+		if(i<0||i>=grid.size()||j<0||j>=grid[0].size())
+			return;
+		if(grid[i][j]=='0')
+			return;
 		grid[i][j] = '0';
-		if (j - 1 >= 0 && grid[i][j - 1] == '1')
-			Recursion(grid, i, j - 1);
-		if (j + 1 < grid[0].size() && grid[i][j + 1] == '1')
-			Recursion(grid, i, j + 1);
-		if (i - 1 >= 0 && grid[i - 1][j] == '1')
-			Recursion(grid, i - 1, j);
-		if (i + 1 < grid.size() && grid[i + 1][j] == '1')
-			Recursion(grid, i + 1, j);
+		dfs(grid, i, j - 1);
+		dfs(grid, i, j + 1);
+		dfs(grid, i - 1, j);
+		dfs(grid, i + 1, j);
 	}
 };
 class Solution1 {
