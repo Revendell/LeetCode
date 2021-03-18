0e925cdeb4fd7f9d8802fb7bc64fbba1e0a50943
Hutao <2312237896@qq.com>
2021-03-18 16:17:08 +0800

    Create 509fibonacci-number.cpp

---
+++ src/509fibonacci-number.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0||n==1)
+            return n;
+        return fib(n-1)+fib(n-2);
+    }
+};
+class Solution1 {
+public:
+    int fib(int n) {
+        if(n==0||n==1)
+            return n;
+        int first=0,second=1;
+        for(int i=2;i<=n;i++)
+        {
+            int tmp=first+second;
+            first=second;
+            second=tmp;
+        }
+        return second;
+    }
+};
+int main()
+{
+    Solution solute;
+    int n=10;
+    cout<<solute.fib(n)<<endl;
+    return 0;
+}
