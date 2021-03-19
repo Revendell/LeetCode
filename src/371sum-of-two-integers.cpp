7f3d97a40852e1c6defbebc8f8c2335b08801018
Hutao <2312237896@qq.com>
2021-03-19 12:19:39 +0800

    Update 371sum-of-two-integers.cpp

--- src/371sum-of-two-integers.cpp
+++ src/371sum-of-two-integers.cpp
@@ -7,6 +7,22 @@ using namespace std;
 class Solution {
 public:
     int getSum(int a, int b) {
+        //迭代：位运算，a^b是无进位的相加;a&b<<1得到每一位的进位
+        int carry,sum;
+        sum=a^b;
+        carry=static_cast<unsigned int>(a&b)<<1;
+        while(carry!=0)
+        {
+            a=sum,b=carry;
+            sum=a^b;
+            carry=static_cast<unsigned int>(a&b)<<1;
+        }
+        return sum;
+    }
+};
+class Solution1 {
+public:
+    int getSum(int a, int b) {
         //基本思想：位运算，a^b是无进位的相加;a&b<<1得到每一位的进位;
         //让无进位相加的结果与进位不断的异或，直到进位为0
         int carry,sum;
