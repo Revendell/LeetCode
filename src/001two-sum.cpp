9acceac1419a43726106fb0f28a0b807ffe6ed05
Hutao <2312237896@qq.com>
2021-05-10 20:11:14 +0800

    Update 001two-sum.cpp

--- src/001two-sum.cpp
+++ src/001two-sum.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_map>
 using namespace std;
 class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+		//基本思想：哈希表
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
+class Solution1 {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
 		vector<int> vec;  //vector容器用于存储之和等于target的两数下标
 		int i=0,j=nums.size()-1,sum;  //两个数的下标i和j分别从容器头和尾检索
+		sort(nums.begin(),nums.end());
 		while(i<j)
 		{
 			sum = nums[i] + nums[j];
