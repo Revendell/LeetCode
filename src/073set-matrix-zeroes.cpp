8318f98d92d2e7d8171e4d7d50c5cf4e1a14cd35
Hutao <2312237896@qq.com>
2021-06-10 20:11:42 +0800

    Update 073set-matrix-zeroes.cpp

--- src/073set-matrix-zeroes.cpp
+++ src/073set-matrix-zeroes.cpp
@@ -6,6 +6,48 @@ using namespace std;
 #define inf -1000000
 class Solution {
 public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int flag_col0 = false, flag_row0 = false;
+        for (int i = 0; i < m; i++) {
+            if (!matrix[i][0]) {
+                flag_col0 = true;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (!matrix[0][j]) {
+                flag_row0 = true;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (!matrix[i][j]) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (!matrix[i][0] || !matrix[0][j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (flag_col0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (flag_row0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+};
+class Solution {
+public:
 	void setZeroes(vector<vector<int>>& matrix) {
 		//基本思想：额外空间O(2)，利用matrix最后一行和最后一列标记当前行和当前列是否有0
 		//如果当前行有0在改行最后一个位置标记为inf，如果当前列有0在改列最后一个位置标记为inf
